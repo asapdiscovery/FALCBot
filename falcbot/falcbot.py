dbb627bcf169ddd8edbfd89d30016c91d780dd06
hmacdope <hugomacdermott@gmail.com>
2024-02-26 14:47:15 +1100

    start of socket mode

--- falcbot/falcbot.py
+++ falcbot/falcbot.py
@@ -1,29 +1,50 @@
-"""Provide the primary functions."""
+import logging
+import re
+
+from pydantic import BaseSettings, Field
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+
+logger = logging.getLogger(__name__)
 
 
-def canvas(with_attribution=True):
-    """
-    Placeholder function to show example docstring (NumPy format).
+class SlackSettings(BaseSettings):
+    SLACK_BOT_TOKEN: str = Field(description="The Slack bot token.", env="SLACK_BOT_TOKEN")
+    SLACK_APP_TOKEN: str = Field(description="The Slack app token.", env="SLACK_APP_TOKEN")
+    # SLACK_SIGNING_SECRET: str = Field(description="The Slack signing secret.")
+    # SLACK_CHANNEL: str = Field(description="The Slack channel to post to.")
 
-    Replace this function and doc string for your own project.
-
-    Parameters
-    ----------
-    with_attribution : bool, Optional, default: True
-        Set whether or not to display who the quote is from.
-
-    Returns
-    -------
-    quote : str
-        Compiled string including quote and optional attribution.
-    """
-
-    quote = "The code is but a canvas to our imagination."
-    if with_attribution:
-        quote += "\n\t- Adapted from Henry David Thoreau"
-    return quote
+settings = SlackSettings()
+app = App(token=settings.SLACK_BOT_TOKEN)
 
 
+@app.message("hello")
+def message_hello(message, say):
+    logger.debug(f"Received a message: {message}")
+    print("messgae received")
+    # say() sends a message to the channel where the event was triggered
+    say(f"Hey there <@{message['user']}>!")
+
+# Start your app
 if __name__ == "__main__":
-    # Do something if this file is invoked on its own
-    print(canvas())
+        print(settings.SLACK_APP_TOKEN)
+        print(settings.SLACK_BOT_TOKEN)
+        SocketModeHandler(app, settings.SLACK_APP_TOKEN).start()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
