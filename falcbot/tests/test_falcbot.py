655d00bd25fecbcfa0c8b7419b4b54d105c52d65
hmacdope <hugomacdermott@gmail.com>
2024-03-28 15:59:59 +1100

    make placeholder test

--- falcbot/tests/test_falcbot.py
+++ falcbot/tests/test_falcbot.py
@@ -2,14 +2,6 @@
 Unit and regression test for the falcbot package.
 """
 
-# Import package, test suite, and other packages as needed
-import sys
 
-import pytest
-
-import falcbot
-
-
-def test_falcbot_imported():
-    """Sample test, will always pass so long as import statement worked."""
-    assert "falcbot" in sys.modules
+def  test_placeholder():
+    assert 1 == 1 
